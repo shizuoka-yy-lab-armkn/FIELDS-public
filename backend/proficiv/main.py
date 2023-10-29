190f120b950e1c1a8f45e48fb5fe7883f9831f03
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-10-30 03:21:55 +0900

    chore: add script for generating openapi.json

--- backend/proficiv/main.py
+++ backend/proficiv/main.py
@@ -1,8 +1,12 @@
+import json
+import sys
 from pathlib import Path
+from typing import IO
 
 import fire
 import uvicorn
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 from proficiv.routers import router
 
@@ -40,10 +44,36 @@ def routes() -> None:
         print(r)
 
 
+def _write_oapi_json(dst: IO, *, indent: int | str | None = None) -> None:
+    json.dump(
+        get_openapi(
+            title=api.title,
+            version=api.version,
+            openapi_version=api.openapi_version,
+            description=api.description,
+            routes=api.routes,
+        ),
+        dst,
+        indent=indent,
+    )
+
+
+def gen_openapi(*, out: str | None = None, indent: int | str | None = None) -> None:
+    if out is None:
+        _write_oapi_json(sys.stdout, indent=indent)
+        return
+
+    with open(out, "w") as f:
+        _write_oapi_json(f, indent=indent)
+
+    print(f"Wrote to {out}.")
+
+
 if __name__ == "__main__":
     fire.Fire(
         {
             "serve": serve,
             "routes": routes,
+            "gen-openapi": gen_openapi,
         }
     )
