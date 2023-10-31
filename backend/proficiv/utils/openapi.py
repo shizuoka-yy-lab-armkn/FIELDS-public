a414fe7f60a1a020d622b271bc4fe78e31ebe748
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-10-31 17:06:32 +0900

    chore(backend): split openapi.json by api version

--- backend/proficiv/utils/openapi.py
+++ backend/proficiv/utils/openapi.py
@@ -13,14 +13,29 @@ def configure_operation_id(api: FastAPI) -> None:
             r.operation_id = stringcase.camelcase(r.name)
 
 
-def write_oapi_json(api: FastAPI, dst: IO, *, indent: int | str | None = None) -> None:
+def write_oapi_json(
+    api: FastAPI,
+    dst: IO,
+    *,
+    filter_path_prefix: str | None = None,
+    indent: int | str | None = None,
+) -> None:
+    routes = api.routes
+
+    if filter_path_prefix is not None:
+        routes = [
+            r
+            for r in routes
+            if isinstance(r, APIRoute) and r.path.startswith(filter_path_prefix)
+        ]
+
     json.dump(
         get_openapi(
             title=api.title,
             version=api.version,
             openapi_version=api.openapi_version,
             description=api.description,
-            routes=api.routes,
+            routes=routes,
         ),
         dst,
         indent=indent,
