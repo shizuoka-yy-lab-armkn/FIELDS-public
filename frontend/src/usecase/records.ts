90f8b1300dd0ffec5a52a7f262e6b16431dc2e0b
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-11-15 03:38:56 +0900

    impl: fmtRecordName

---
+++ frontend/src/usecase/records.ts
@@ -0,0 +1,7 @@
+import * as schema from "@/gen/oapi/backend/v1/schema";
+import { fmtDatetime } from "@/utils/datetime";
+
+export const fmtRecordName = (record: schema.Record): string => {
+  const d = new Date(record.recordAt);
+  return `収録 ${record.seq} - ${fmtDatetime(d)}`;
+};
