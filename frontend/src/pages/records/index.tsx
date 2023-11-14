87286be81ae2363815e7191beeba90dcf3a03e69
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-11-15 03:47:57 +0900

    feat: record list page

---
+++ frontend/src/pages/records/index.tsx
@@ -0,0 +1,46 @@
+import { WithHeader } from "@/components/layout/WithHeader";
+import { ChakraNextLink } from "@/components/util/ChakraNextLink";
+import { useGetRecordList } from "@/hooks/records";
+import { fmtRecordName } from "@/usecase/records";
+import { Box, Container, Heading } from "@chakra-ui/react";
+
+export default function RecordListPage() {
+  const { records } = useGetRecordList();
+  return (
+    <WithHeader>
+      <Container maxW="720px" minH="full">
+        <Heading as="h1" mt={8} mb={4} color="teal.900">収録履歴</Heading>
+        <Box
+          as="ul"
+          listStyleType="none"
+          display="flex"
+          flexDir="column"
+          gap={4}
+        >
+          {records?.map((r) => (
+            <Box
+              as="li"
+              key={r.recordId}
+              bg="white"
+              borderRadius={4}
+            >
+              <ChakraNextLink
+                href={`/records/${r.recordId}`}
+                display="block"
+                px={4}
+                py={3}
+                color="teal.900"
+                _hover={{
+                  bg: "gray.50",
+                  color: "cyan.500",
+                }}
+              >
+                {fmtRecordName(r)}
+              </ChakraNextLink>
+            </Box>
+          ))}
+        </Box>
+      </Container>
+    </WithHeader>
+  );
+}
