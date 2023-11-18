48e47b1c17a3a4e614129fd992da5fb826049287
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-11-18 12:56:50 +0900

    feat: GET /subjects/{subject_id}

--- backend/proficiv/routers/v1/subjects.py
+++ backend/proficiv/routers/v1/subjects.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from proficiv.domain.subjects.schema import Subject
-from proficiv.entity import SubjectID
+from proficiv.db import prisma
+from proficiv.domain.subjects.schema import ActionMeta, Subject
+from proficiv.entity import ActionID, SubjectID
 
 router = APIRouter(
     prefix="/subjects",
@@ -11,5 +12,28 @@ router = APIRouter(
 
 @router.get("/{subject_id}")
 async def get_subject(subject_id: SubjectID) -> Subject:
-    del subject_id
-    raise NotImplementedError()
+    subj = await prisma.subject.find_unique(
+        where={"id": subject_id},
+        include={"actions": True},
+    )
+
+    if subj is None:
+        raise HTTPException(404)
+
+    assert subj.actions is not None
+    actions = [
+        ActionMeta(
+            action_id=ActionID(a.id),
+            short_name=a.short_name,
+            long_name=a.long_name,
+            manual_markdown="",
+            master_dur_mean=a.master_dur_mean,
+            master_dur_std=a.master_dur_std,
+            master_dur_min=a.master_dur_min,
+            master_dur_max=a.master_dur_max,
+            master_dur_median=a.master_dur_median,
+        )
+        for a in subj.actions
+    ]
+
+    return Subject(id=SubjectID(subj.id), name=subj.name, actions=actions)
