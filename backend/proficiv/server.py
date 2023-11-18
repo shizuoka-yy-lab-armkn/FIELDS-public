a2dad1ca0638280bba198e68d91e9c9dd467bfbd
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-11-18 12:53:33 +0900

    impl: manage db connection/disconnection using fastapi lifespan

--- backend/proficiv/server.py
+++ backend/proficiv/server.py
@@ -1,10 +1,26 @@
+from contextlib import asynccontextmanager
+from typing import Any, AsyncGenerator
+
 from fastapi import FastAPI
 
+from proficiv.config import get_config
+from proficiv.db import prisma
 from proficiv.routers import router
 from proficiv.utils.openapi import configure_operation_id
 
+
+@asynccontextmanager
+async def _lifespan(_: FastAPI) -> AsyncGenerator[None, Any]:
+    cfg = get_config()
+    prisma._log_queries = cfg.debug
+    await prisma.connect()
+    yield
+    await prisma.disconnect()
+
+
 api = FastAPI(
     title="Proficiv Backend API",
+    lifespan=_lifespan,
     swagger_ui_parameters={
         "displayRequestDuration": True,
         "displayOperationId": True,
