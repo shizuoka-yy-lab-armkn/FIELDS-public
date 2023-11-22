147f4e3753d9f51ba11dcda26e13d6bfdb1d7195
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-11-22 17:51:18 +0900

    chore: log actions on evaluation api

--- backend/proficiv/routers/v1/records.py
+++ backend/proficiv/routers/v1/records.py
@@ -79,7 +79,6 @@ async def get_record_evaluation(
 
     recog_action_seqs = [aid2a[s.action_id].seq for s in recog_segs]
     master_action_seqs = [m.seq for m in master_actions]
-
     # NOTE: 工程19, 18 は模範の順番が逆転している
     p18 = master_action_seqs.index(18)
     p19 = master_action_seqs.index(19)
@@ -87,7 +86,11 @@ async def get_record_evaluation(
         master_action_seqs[p19],
         master_action_seqs[p18],
     )
+    _log.info(f"\n{recog_action_seqs=}")
+    _log.info(f"\n{master_action_seqs=}")
+
     matchings = compare_action_seq_by_lcs(src=recog_action_seqs, tgt=master_action_seqs)
+    _log.info(f"\n{matchings=}")
 
     eval_segs: list[Segment] = []
     for m in matchings:
